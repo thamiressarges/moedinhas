58ed3bff830c962c11f03e29cc543a8fd0126d51
Thamires Sarges <thamires.sarges.58@gmail.com>
2026-08-09 23:59:35 -0400

    refactor: utiliza fonte Inter nos estilos de texto

--- lib/common/constants/app_text_styles.dart
+++ lib/common/constants/app_text_styles.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppTextStyles {
   AppTextStyles._();
 
-  static final TextStyle bigText = TextStyle(
-    fontSize: 50.0,
-    fontWeight: FontWeight.bold
+  static final TextStyle bigText = GoogleFonts.inter(
+    fontSize: 50,
+    fontWeight: FontWeight.w700
   );
+
+
+
 
 }
