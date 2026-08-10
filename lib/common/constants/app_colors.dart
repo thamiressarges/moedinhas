f3a5f52f5d63f0d3a146ded38d2a56ef59dfb5ac
Thamires Sarges <thamires.sarges.58@gmail.com>
2026-08-09 23:43:39 -0400

    refactor: centraliza estilos de texto em AppTextStyles

---
+++ lib/common/constants/app_colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class AppColors{
+  AppColors._();
+
+  static const Color greenLightOne = Color(0xff63b5af);
+  static const Color greenLightTwo = Color(0xff438883);
+  static const List<Color> greenGradient = [greenLightOne, greenLightTwo] ;
+  static const Color white = Color(0xffFFFFFF);
+}
---
+++ lib/common/constants/app_text_styles.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  AppTextStyles._();
+
+  static final TextStyle bigText = TextStyle(
+    fontSize: 50.0,
+    fontWeight: FontWeight.bold
+  );
+
+}
